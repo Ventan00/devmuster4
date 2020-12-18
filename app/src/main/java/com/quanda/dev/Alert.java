1d11d5a8d32868ab0ea365852f638d67896a89f5
Ventan00 <Ventantogame@gmail.com>
2020-12-18 19:01:34 +0100

    Update Alert.java

--- app/src/main/java/com/quanda/dev/Alert.java
+++ app/src/main/java/com/quanda/dev/Alert.java
@@ -3,6 +3,7 @@ package com.quanda.dev;
 import android.view.View;
 import com.google.android.material.snackbar.Snackbar;
 
+//test
 public class Alert {
     public static void show(View view, String message){
         Snackbar snackbar = Snackbar.make(view, message, 2000);
